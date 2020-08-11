c1b940fc16240b41c9b9d291b4cb7363d1481350
Nibesh Kiran Adhikari <menibesh@gmail.com>
2020-08-11 23:41:14 +0545

    create get previous block method

--- module 1/blockchain.py
+++ module 1/blockchain.py
@@ -20,3 +20,6 @@ class Blockchain:
                 'previuos_hash' : prvious_hash }
         self.chain.append(block)
         return block
+    
+def get_previous_block(self):
+    return self.chain [-1]
